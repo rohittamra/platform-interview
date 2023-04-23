0c4c9cd33a11f88eb66d9afdc1c23e06787b33a4
root <root@testvm1.pifasita1jee3e0x1rb0oblekh.zrhx.internal.cloudapp.net>
2023-04-23 16:28:25 +0000

    Changes

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-trap 'popd' EXIT
 
 set -e
 
+current_dir=`pwd`
 echo Installing docker-compose
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
@@ -17,20 +17,22 @@ echo "1st ${HOME}/bin"
 mkdir -p "${HOME}/bin"
 sudo apt-get update && sudo apt-get install -y unzip jq
 echo "2nd 2nd ${HOME}/bin"
-pushd  "${HOME}/bin"
+cd  "${HOME}/bin"
 
 wget -q "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_${pro}.zip"
 unzip -q -o "terraform_${terraform_version}_linux_${pro}.zip"
-popd
-pushd /vagrant
-docker build ./services/account -t form3tech-oss/platformtest-account
-docker build ./services/gateway -t form3tech-oss/platformtest-gateway
-docker build ./services/payment -t form3tech-oss/platformtest-payment
+cd $current_dir
+echo "Switched back to the previous directory"
+echo "Building Docker images..."
+#cd /vagrant
+echo "Switched to /vagrant directory"
+ls -la
+#docker build ./services/account -t form3tech-oss/platformtest-account
+#docker build ./services/gateway -t form3tech-oss/platformtest-gateway
+#docker build ./services/payment -t form3tech-oss/platformtest-payment
 docker-compose up -d
-popd
 echo Applying terraform script
-pushd /vagrant/tf
-cd ./tf
+cd $current_dir/tf
 terraform init -upgrade
 terraform apply -auto-approve
 popd
