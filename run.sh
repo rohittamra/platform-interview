54e50f721bdd5a1cdf657d441553b831b718a000
root <root@testvm1.pifasita1jee3e0x1rb0oblekh.zrhx.internal.cloudapp.net>
2023-04-23 16:04:57 +0000

    Changes

--- run.sh
+++ run.sh
@@ -1,34 +1,36 @@
-#!/bin/bash
-
-#trap 'popd' EXIT
+#!/usr/bin/env bash
+trap 'popd' EXIT
 
 set -e
 
 echo Installing docker-compose
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
-pro=$(uname -n)
+pro=$(dpkg --print-architecture)
 terraform_version="1.2.5"
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-${os}-${arch}" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+. "${HOME}/.profile"
 echo Installing terraform onto machine...
+echo "1st ${HOME}/bin"
 mkdir -p "${HOME}/bin"
 sudo apt-get update && sudo apt-get install -y unzip jq
-cd "${HOME}/bin"
+echo "2nd 2nd ${HOME}/bin"
+pushd  "${HOME}/bin"
 
 wget -q "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_${pro}.zip"
 unzip -q -o "terraform_${terraform_version}_linux_${pro}.zip"
-. "${HOME}/.profile"
-cd -
-cd /vagrant
+popd
+pushd /vagrant
 docker build ./services/account -t form3tech-oss/platformtest-account
 docker build ./services/gateway -t form3tech-oss/platformtest-gateway
 docker build ./services/payment -t form3tech-oss/platformtest-payment
 docker-compose up -d
-cd -
+popd
 echo Applying terraform script
-cd /vagrant/tf
+pushd /vagrant/tf
+cd ./tf
 terraform init -upgrade
 terraform apply -auto-approve
-cd -
+popd
